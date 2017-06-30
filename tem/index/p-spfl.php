1b41e8b2091d2ea81fa4f267381718aec76bc1d0
Martinqi <www.965593718@qq.com>
2017-06-30 23:38:02 +0800

    'peng'

---
+++ tem/index/p-spfl.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/6/30
+ * Time: 21:45
+ */
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../static/css/common.css">
+    <link rel="stylesheet" href="../../static/css/p-spfl.css">
+    <script src="../../static/js/iscroll.js"></script>
+    <script type="text/javascript" src="../../static/js/demoUtils.js"></script>
+</head>
+<body>
+<div class="zhezhao"></div>
+<div class="top">
+    <p style="font-size: 15px">分类</p>
+    <p style="font-size: 12px">SHAPFENLEI</p>
+</div>
+<div style="height: 50px;width: 100%"></div>
+<div class="list">
+    <div class="box">
+        <img src="../../static/img/p-f1_03.png" alt="">
+        <p>#营养搭配</p>
+        <p>weinindeshenghuobaojiahuhang</p>
+        <div class="num">1.</div>
+    </div>
+    <div class="box">
+        <img src="../../static/img/p-f2_03.png" alt="">
+        <p>#所有商品</p>
+        <p>suoyoushangpingkjssadv</p>
+        <div class="num">2.</div>
+    </div>
+    <div class="box">
+        <img src="../../static/img/p-f3_03.png" alt="">
+        <p>#热带水果</p>
+        <p>redaishuiguozhenhaochi</p>
+        <div class="num">3.</div>
+    </div>
+    <div class="box">
+        <img src="../../static/img/p-f4_03.png" alt="">
+        <p>#进口水果</p>
+        <p>jinkoushuiguobuzai</p>
+        <div class="num">4.</div>
+    </div>
+</div>
+</body>
+</html>
