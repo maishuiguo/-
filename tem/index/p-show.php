ea3a5c58eeb74cc37e8c216b57983e923faccbeb
Martinqi <www.965593718@qq.com>
2017-07-06 10:32:30 +0800

    'sdasd'

--- tem/index/p-show.php
+++ tem/index/p-show.php
@@ -16,8 +16,11 @@
     <title>Document</title>
     <link rel="stylesheet" href="../../static/css/common.css">
     <link rel="stylesheet" href="../../static/css/p-show.css">
+    <link rel="stylesheet" href="../../static/css/swiper.min.css">
+    <script src="../../static/js/iscroll.js"></script>
     <script src="../../static/js/common.js"></script>
-    <script src="../../static/js/common.js"></script>
+    <script src="../../static/js/swiper.min.js"></script>
+    <script src="../../static/js/p-show.js"></script>
 </head>
 <body>
 <div class="top">
@@ -28,26 +31,31 @@
 <div style="width: 100%;height: 0.97rem"></div>
 <div id="wrapper">
     <div id="scroller">
-        <ul>
-            <li>
-                <div class="banner" style="background-image: url('../../static/img/p-b1.png');background-size: cover">
-                    <img src="../../static/img/p-tiao.png">
+                <div class="banner">
+                    <div class="swiper-container">
+                        <div class="swiper-wrapper">
+                            <div style="background-image: url('../../static/img/p-b1.png');background-size: cover" class="swiper-slide"></div>
+                            <div style="background-image: url('../../static/img/p-b1.png');background-size: cover" class="swiper-slide"></div>
+                            <div style="background-image: url('../../static/img/p-b1.png');background-size: cover" class="swiper-slide"></div>
+                        </div>
+                        <!-- Add Pagination -->
+                        <!-- Add Arrows -->
+                        <div style="background-image:url('../../static/img/p-right.png');width: 0.46rem;height: 0.46rem;border-radius: 0.46rem" class="swiper-button-next"></div>
+                        <div style="background-image:url('../../static/img/p-left .png');width: 0.46rem;height: 0.46rem;border-radius: 0.46rem"  class="swiper-button-prev"></div>
+                    </div>
+                    <img class="tiao" src="../../static/img/p-tiao.png">
                     <p class="one">细腻多汁 甜度高于红心橙</p>
                     <div class="jianjie">
                         <p>商品简介：甜而不腻，其水分
                             更多，口感好清淡中透着香，越吃越香。</p>
                     </div>
                 </div>
-            </li>
-            <li>
-
                 <div class="xq">
                     <img src="../../static/img/p-xq.png">
                     <p>红心橙</p>
                     <p>HONGXiNCHENG</p>
                     <p>月销量：362 邮费包邮 各地区均有货--送至山西太原</p>
-                </div>   </li>
-            <li>
+                </div>
                 <div class="pinglun" style="background-image: url('../../static/img/p-xqbg.png');background-size: cover">
                     <ul>
                         <li>
@@ -84,86 +92,85 @@
                         </li>
                     </ul>
                 </div>
-            </li>
-                <li><div class="cnxh">
+              <div class="cnxh">
                     <img src="../../static/img/p-cnxh.png">
-                    </div></li>
-               <li><div class="tuijian">
-                    <div class="list" style="background-image: url('../../static/img/p-tuijian.png');background-size: cover">
-                        <div class="zhezhao">
-                            <p>红草莓</p>
-                            <p>HONGCAOMEI</p>
-                        </div>
                     </div>
-                    <div class="list" style="background-image: url('../../static/img/p-tuijian.png');background-size: cover">
-                        <div class="zhezhao">
-                            <p>红草莓</p>
-                            <p>HONGCAOMEI</p>
-                        </div>
-                    </div>
-                    <div class="list" style="background-image: url('../../static/img/p-tuijian.png');background-size: cover">
-                        <div class="zhezhao">
-                            <p>红草莓</p>
-                            <p>HONGCAOMEI</p>
-                        </div>
-                    </div>
-                    <div class="hong"></div>
-                    <div class="bottom">
-                        <div class="box">
-                            <img src="../../static/img/p-jian.png">
-                            <b>0</b>
-                            <img src="../../static/img/p-jia.png">
-                        </div>
-                        <div class="box">
-                            <button class="one">
-                                立即购买
-                            </button>
-                            <button class="two">
-                                加入购物车
-                            </button>
-                        </div>
-                    </div>
-                    <div class="list1">
-                        <span><b>35.2</b> RMB</span>
-                        <span style="float: right;text-decoration: line-through"><b style="color:#000;">50.2</b> RMB</span>
-                    </div>
-                    <div class="list1">
-                        <span><b>35.2</b> RMB</span>
-                        <span style="float: right;text-decoration: line-through"><b style="color:#000;">50.2</b> RMB</span>
-                    </div>
-                    <div class="list1">
-                        <span><b>35.2</b> RMB</span>
-                        <span style="float: right;text-decoration: line-through"><b style="color:#000;">50.2</b> RMB</span>
-                    </div>
-                    <div class="list2">
-                        了解详情
-                    </div>
-                    <div class="list2">
-                        了解详情
-                    </div>
-                    <div class="list2">
-                        了解详情
-                    </div>
-                </div></li>
-
-        </ul>
-
+        <div class="tuijian">
+            <div class="list" style="background-image: url('../../static/img/p-tuijian.png');background-size: cover">
+                <div class="zhezhao">
+                    <p>红草莓</p>
+                    <p>HONGCAOMEI</p>
+                </div>
+            </div>
+            <div class="list" style="background-image: url('../../static/img/p-tuijian.png');background-size: cover">
+                <div class="zhezhao">
+                    <p>红草莓</p>
+                    <p>HONGCAOMEI</p>
+                </div>
+            </div>
+            <div class="list" style="background-image: url('../../static/img/p-tuijian.png');background-size: cover">
+                <div class="zhezhao">
+                    <p>红草莓</p>
+                    <p>HONGCAOMEI</p>
+                </div>
+            </div>
+            <div class="hong"></div>
+            <div class="list1">
+                <span><b>35.2</b> RMB</span>
+                <span style="float: right;text-decoration: line-through"><b style="color:#000;">50.2</b> RMB</span>
+            </div>
+            <div class="list1">
+                <span><b>35.2</b> RMB</span>
+                <span style="float: right;text-decoration: line-through"><b style="color:#000;">50.2</b> RMB</span>
+            </div>
+            <div class="list1">
+                <span><b>35.2</b> RMB</span>
+                <span style="float: right;text-decoration: line-through"><b style="color:#000;">50.2</b> RMB</span>
+            </div>
+            <div class="list2">
+                了解详情
+            </div>
+            <div class="list2">
+                了解详情
+            </div>
+            <div class="list2">
+                了解详情
+            </div>
+        </div>
+    </div>
+    </div >
+<div class="bottom">
+    <div class="box">
+        <img src="../../static/img/p-jian.png">
+        <b>0</b>
+        <img src="../../static/img/p-jia.png">
+    </div>
+    <div class="box">
+        <button class="one">
+            立即购买
+        </button>
+        <button class="two">
+            加入购物车
+        </button>
     </div>
 </div>
 </body>
 </html>
 
 <script type="text/javascript">
+    var myScroll = new IScroll('#wrapper', {
+        mousewheel:true,
+        click:true,
+    });
 
-    var myScroll;
-
-    function loaded () {
-        myScroll = new IScroll('#wrapper', { keyBindings: true });
-    }
-
-    document.addEventListener('touchmove', function (e) { e.preventDefault(); }, isPassive() ? {
-        capture: false,
-        passive: false
-    } : false);
-
+//轮播
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        paginationClickable: true,
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplayDisableOnInteraction: false
+    });
 </script>
