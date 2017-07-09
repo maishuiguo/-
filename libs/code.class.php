bd7814ce627b68e513b9accd5b167cbe3e300d71
houyangy <18435106280@163.com>
2017-07-10 05:44:33 +0800

    sss

--- libs/code.class.php
+++ libs/code.class.php
@@ -1,112 +1,92 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/6/27 0027
+ * Time: 16:01
+ */
+
 class code{
-    public $width=200;
-    public $height=40;
+    public $letterarr="2345678abcdefhjkmnprstuvwxyABCDEFHJKMNPRSTUVWXY";
+    public $letterlen=4;
+    public $width=100;
+    public $height=60;
+    public $img;
     public $type="png";
-    public $code="2345678abcdefhjkmnpABCDEFGHJKMN";
-    public $codeLen=4;
-    public $codeUrl;
-    private $image;
     public $current="";
-    public $size=array("min"=>15,"max"=>30);
-    public $angle=array("min"=>-15,"max"=>15);
-    public $lineNum=10;
-    public $pixNum=50;
+    public $size=array("min"=>20,"max"=>35);
+    public $angle=array("min"=>-10,"max"=>10);
+    public $fonturl="static/font/ARCENA.ttf";
+    public $linenum=3;
+    public $pointnum=100;
 
-
-    //1. 创建画布
-    function createCanvas(){
-          $this->image=imagecreatetruecolor($this->width,$this->height);
-          imagefill($this->image,0,0,$this->getColor());
-
+    function createimg(){
+        $this->img=imagecreatetruecolor($this->width,$this->height);
+//        $color=$this->getcolor('q');
+        $color=imagecolorallocate($this->img,197,197,197);
+        imagefill($this->img,0,0,$color);
     }
 
-    //2.  创建内容  abcd
-    function createCon(){
-
-          $this->getText();
-
-        //随机
-        for($i=0;$i<$this->codeLen;$i++){
+    function createcon(){
+        $this->gettext();
+        for($i=0;$i<$this->letterlen;$i++){
             $size=mt_rand($this->size["min"],$this->size["max"]);
             $angle=mt_rand($this->angle["min"],$this->angle["max"]);
-            $x=$i*($this->width/$this->codeLen)+mt_rand(-10,10);
-            if($i==0){
-                $x=$x<0?0:$x;
-            }
-            $box=imagettfbbox($size,$angle,$this->codeUrl,$this->current[$i]);
-            $height=$box[1]-$box[5];
-            $y=$box[1]-$box[5]+mt_rand(-10,20);
-
-            $y=$y<$height?$height:$y;
-            imagettftext($this->image,$size,$angle,$x,$y,$this->getColor("sad"),$this->codeUrl,$this->current[$i]);
+            $x=$i*($this->width/$this->letterlen)+mt_rand(0,8);
+            $bbox=imagettfbbox($size,$angle,$this->fonturl,$this->current[$i]);
+            $height=$bbox[1]-$bbox[5];
+            $y=$height+mt_rand(0,10);
+            $color=$this->getcolor();
+            imagettftext($this->img,$size,$angle,$x,$y,$color,$this->fonturl,$this->current[$i]);
         }
     }
 
-
-    //创建出随机的文字
-    function getText(){
-
-        $str="";
-        for($i=0;$i<$this->codeLen;$i++) {
-            $num = mt_rand(0, strlen($this->code)-1);
-            $str.=$this->code[$num];
+    function createline(){
+        for($i=0;$i<$this->linenum;$i++){
+            $color=$this->getcolor("q");
+            imageline($this->img,mt_rand(0,$this->width/2),mt_rand(0,$this->height),mt_rand($this->width/2,$this->width),mt_rand(0,$this->height),$color);
         }
+    }
+    function createpoint(){
+        for($i=0;$i<$this->pointnum;$i++){
+            $color=$this->getcolor("s");
+            imagesetpixel($this->img,mt_rand(0,$this->width),mt_rand(0,$this->height),$color);
+        }
+    }
 
+    function getstr(){
+        $this->current=strtolower($this->current);
+//        $this->session->set("code",$this->current);
+        session_start();
+        $_SESSION['code']=$this->current;
+    }
+    function getcolor($type="s"){
+        $r=$type=="s"?mt_rand(0,120):mt_rand(125,255);
+        $g=$type=="s"?mt_rand(0,120):mt_rand(125,255);
+        $b=$type=="s"?mt_rand(0,120):mt_rand(125,255);
+        return imagecolorallocate($this->img,$r,$g,$b);
+    }
+    function gettext(){
+        $str="";
+        for($i=0;$i<$this->letterlen;$i++){
+            $str.=substr($this->letterarr,mt_rand(0,strlen($this->letterarr)-1),1);
+        }
         $this->current=$str;
         return $str;
+
     }
-
-
-    //3. 创建线条
-
-    function createLine(){
-        for($i=0;$i<$this->lineNum;$i++){
-            imageline($this->image,mt_rand(0,$this->width/2),mt_rand(0,$this->height),mt_rand($this->width/2,$this->width),mt_rand(0,$this->height),$this->getColor("back"));
-        }
-    }
-
-    //4.  创建点
-
-    function createPix(){
-        for($i=0;$i<$this->pixNum;$i++){
-            imagesetpixel($this->image,mt_rand(0,$this->width),mt_rand(0,$this->height),$this->getColor("jshd"));
-        }
-    }
-
-
-
-
-
-    function getColor($type="back"){
-        $r=$type=="back"?mt_rand(0,120):mt_rand(125,255);
-        $g=$type=="back"?mt_rand(0,120):mt_rand(125,255);
-        $b=$type=="back"?mt_rand(0,120):mt_rand(125,255);
-       return imagecolorallocate($this->image,$r,$g,$b);
-    }
-
-
-
     function output(){
-
-        header("content-type:image/".$this->type);
-        //1. 创建画布
-        $this->createCanvas();
-        //2.  创建文字
-        $this->createCon();
-
-        //3.  创建干扰的线
-        $this->createLine();
-
-        //4.  创建干扰点
-
-        $this->createPix();
+        header("content-type:image/".$this->type.";charset=utf-8");
+        $this->createimg();
+        $this->createline();
+        $this->createpoint();
+        $this->createcon();
         $type="image".$this->type;
-        $type($this->image);
-
-        $this->current=strtolower($this->current);
-        imagedestroy($this->image);
-
+        $type($this->img);
+        imagedestroy($this->img);
     }
 }
+//$code=new code();
+//$code->fonturl="../static/font/ARCENA.ttf";
+//$code->output();
 
