6ba42f152ba51843bbcf9dda563c9287d783140a
Erossssss <you@example.com>
2017-07-14 12:52:09 +0800

    1

--- com/48f16a32fe88b6b84816c4c83e67a61b3a80b7c5_0.file.zcy-bank.html.php
+++ com/48f16a32fe88b6b84816c4c83e67a61b3a80b7c5_0.file.zcy-bank.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2017-07-10 03:36:10
+/* Smarty version 3.1.30, created on 2017-07-11 05:06:01
   from "F:\Program Files\soft\wamp\www\shuiguoApp\-\tem\index\zcy-bank.html" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_5962da0a2e5c90_48367320',
+  'unifunc' => 'content_596440990b6bc5_87327207',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '48f16a32fe88b6b84816c4c83e67a61b3a80b7c5' => 
     array (
       0 => 'F:\\Program Files\\soft\\wamp\\www\\shuiguoApp\\-\\tem\\index\\zcy-bank.html',
-      1 => 1499592762,
+      1 => 1499742064,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5962da0a2e5c90_48367320 (Smarty_Internal_Template $_smarty_tpl) {
+function content_596440990b6bc5_87327207 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -49,20 +49,42 @@ zcy-bank.css">
         <div class="num3">03</div>
     </div>
     <div class="img">
-        <a href="index.php?m=index&f=pay&a=end">
-            <img src="<?php echo IMG_PATH;?>
-zcy-img/8.png" alt="">
-        </a>
-        <a href="index.php?m=index&f=pay&a=end">
-            <img src="<?php echo IMG_PATH;?>
-zcy-img/9.png" alt="">
-        </a>
-        <a href="index.php?m=index&f=pay&a=end">
-            <img src="<?php echo IMG_PATH;?>
-zcy-img/10.png" alt="">
-        </a>
-
+        <a href="#" class="md-trigger" data-modal="modal-1"><img src="<?php echo IMG_PATH;?>
+zcy-img/8.png" alt=""></a>
+        <a href="#" class="md-trigger" data-modal="modal-1"><img src="<?php echo IMG_PATH;?>
+zcy-img/9.png" alt=""></a>
+        <a href="#" class="md-trigger" data-modal="modal-1"><img src="<?php echo IMG_PATH;?>
+zcy-img/10.png" alt=""></a>
     </div>
+    <div class="md-modal md-effect-16" id="modal-1">
+        <div class="md-content">
+            <div class="white">
+                <img src="<?php echo IMG_PATH;?>
+zcy-img/1.png" alt="">
+                <div class="money">83.40</div>
+                <form action="" method="post" class="lemo">
+                    <p style="margin-top: .2rem">马来西亚红心柠檬 &nbsp; 3个</p>
+                    <p>地址：太原市小店区康宁街26号</p>
+                    <p>订单号：2201 7095 5468 7894</p>
+                    <h1 style="margin-top: .2rem">输入支付密码</h1>
+                    <p>
+                        <span class="pay">PAYMENT</span><span class="pay1">PASSWORD</span>
+                    </p>
+                    <input type="password" id="pass">
+                </form>
+            </div>
+            <a href="index.php?m=index&f=pay&a=end" class="md-close">立即支付</a>
+        </div>
+    </div>
+    <div class="md-overlay"></div>
+    <?php echo '<script'; ?>
+ src="<?php echo JS_PATH;?>
+classie.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="<?php echo JS_PATH;?>
+modalEffects.js"><?php echo '</script'; ?>
+>
 </body>
 </html><?php }
 }
---
+++ com/e85a39da833153ec5f41f40af4f491eaf3f0c884_0.file.zcy-end.html.php
@@ -0,0 +1,62 @@
+<?php
+/* Smarty version 3.1.30, created on 2017-07-11 05:06:04
+  from "F:\Program Files\soft\wamp\www\shuiguoApp\-\tem\index\zcy-end.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_5964409ccc2189_35067781',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'e85a39da833153ec5f41f40af4f491eaf3f0c884' => 
+    array (
+      0 => 'F:\\Program Files\\soft\\wamp\\www\\shuiguoApp\\-\\tem\\index\\zcy-end.html',
+      1 => 1499592762,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5964409ccc2189_35067781 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php echo '<script'; ?>
+ src="<?php echo JS_PATH;?>
+common.js"><?php echo '</script'; ?>
+>
+    <link rel="stylesheet" href="<?php echo CSS_PATH;?>
+zcy-end.css">
+</head>
+<body>
+<div class="box">
+    <a href="javascript:history.go(-1);">
+        <img src="<?php echo IMG_PATH;?>
+zcy-img/11.png" alt="" class="go">
+    </a>
+    <div class="title">
+        <p>·结算页面·</p>
+        <p>SETTLEMENT PAGE</p>
+    </div>
+</div>
+    <div class="img">
+        <img src="<?php echo IMG_PATH;?>
+zcy-img/12-2.png" class="img1">
+        <img src="<?php echo IMG_PATH;?>
+zcy-img/13.png" class="img2">
+        <img src="<?php echo IMG_PATH;?>
+zcy-img/14.png" class="img3">
+        <a id="point"></a>
+    </div>
+</body>
+</html><?php }
+}
